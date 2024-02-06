cf8659d059000e28a7a71c50cc805ed82390bda4
Dani Arthur Widodo <work.daniarthurwidodo@gmail.com>
2024-02-06 15:13:40 +0700

    fix

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const https = require("https"),
  fs = require("fs");
 
 const options = {
-  key: fs.readFileSync("../private.key"),
-  cert: fs.readFileSync("../ca_bundle.crt")
+  key: fs.readFileSync("/etc/ssl/private/private.key"),
+  cert: fs.readFileSync("/etc/ssl/certs/ca_bundle.crt")
 };
 https.createServer(options, app).listen(8082);
